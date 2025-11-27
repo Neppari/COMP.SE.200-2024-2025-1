f46dcf98e60380767178828e8de3e27d9676b661
werticalz <57874449+werticalz@users.noreply.github.com>
2025-11-27 18:51:30 +0200

    Clamp not working as expected in documentation

--- test/clamp.test.js
+++ test/clamp.test.js
@@ -1,1 +1,8 @@
 import clamp from '../src/clamp.js';
+
+describe('clamp', () => {
+    test('should clamp number within the specified range', () => {
+        expect(clamp(-10, -5, 5)).toBe(-5);
+        expect(clamp(10, -5, 5)).toBe(5);
+    });
+});
