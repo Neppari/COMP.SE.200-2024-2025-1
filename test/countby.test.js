e27079abcb9ba9204b33d421da1d1baff9c01c02
werticalz <57874449+werticalz@users.noreply.github.com>
2025-11-27 18:47:48 +0200

    changed countBy.js to work as described and to pass simple test described in comment.

--- test/countby.test.js
+++ test/countby.test.js
@@ -1,1 +1,13 @@
 import countBy from '../src/countby.js';
+
+describe('countBy', () => {
+    test("should handle example case correctly", () => {
+        const users = [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'betty', 'active': true },
+            { 'user': 'fred', 'active': false }
+        ];
+        const result = countBy(users, value => value.active);
+        expect(result).toEqual({ 'true': 2, 'false': 1 });
+    });
+});
