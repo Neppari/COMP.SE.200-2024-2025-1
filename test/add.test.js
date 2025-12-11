c254b2776e5abf2ea9d5b4b96f75b8b0d37457ce
Meri Mattila <meri-mattila@outlook.com>
2025-12-11 11:27:11 +0200

    added tests for add function

---
+++ test/add.test.js
@@ -0,0 +1,35 @@
+import add from '../src/add.js';
+
+describe('add tests', () => {
+  test('adds two numbers together', () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  test('adds two negative numbers together', () => {
+    expect(add(-2, -3)).toBe(-5);
+  });
+
+  test('adds a positive and a negative number together', () => {
+    expect(add(2, -3)).toBe(-1);
+  });
+
+  test('add floating point and integer together', () => {
+    expect(add(1.5, 2)).toBe(3.5);
+  });
+
+  test('add zero and a number together', () => {
+    expect(add(0, 2)).toBe(2);
+  });
+
+  test('add minus zero and a number together', () => {
+    expect(add(-0, 2)).toBe(2);
+  });
+
+  test('adds two strings together throws TypeError', () => {
+    expect(() => add("hello", "world")).toThrow(TypeError);
+  });
+
+  test('adds a number and a string together throws TypeError', () => {
+    expect(() => add(2, "3")).toThrow(TypeError);
+  });
+});
